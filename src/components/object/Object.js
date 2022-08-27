09c6d00785c1ccacd91b66be73557042dc290c7d
Afeefa-Batool <99643705+Afeefa-Batool@users.noreply.github.com>
2022-08-27 11:19:41 -0700

    props

--- src/components/object/Object.js
+++ src/components/object/Object.js
@@ -1,27 +1,26 @@
-import React from 'react';
-import { Table } from 'react-bootstrap';
+import React from "react";
+import { Table } from "react-bootstrap";
 
 const Object = (props) => {
-    // destructuring a object
-    let { firstName, lastName,age,eyeColor } =props.person;
-// console.log(firstName)
-// console.log(eyeColor);
-// object py map nhi chalta
-    return (
-        <div>
-          <Table striped bordered hover>
-      <thead>
-        <tr>
-          <th>{firstName}</th>
-          <th>{lastName}</th>
-          <th>{age}</th>
-          <th>{eyeColor}</th>
-        </tr>
-      </thead>
+  // destructuring a object
+  let { firstName, lastName, age, eyeColor } = props.person;
+  // console.log(firstName)
+  // console.log(eyeColor);
+  // object py map nhi chalta
+  return (
+    <div>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>{firstName}</th>
+            <th>{lastName}</th>
+            <th>{age}</th>
+            <th>{eyeColor}</th>
+          </tr>
+        </thead>
       </Table>
-          
-        </div>
-    );
+    </div>
+  );
 };
 
 export default Object;
